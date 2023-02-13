96d0f93bc1bf5975f4e1ab08d14ed1d1b7e560b4
HARISRUJAN <bannusrujan2605@gmail.com>
2023-02-13 10:19:45 +0530

    psycopg removed

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ s3 = boto3.client('s3',
                     aws_access_key_id='ASIAYPIF54RQHC6QVCUL',
                     aws_secret_access_key= "ieA5jlhtaTFe0FWUfLQKFW8Dk8DML+TwO8gK3NNd",
                     aws_session_token="IQoJb3JpZ2luX2VjEBoaCmFwLXNvdXRoLTEiSDBGAiEAks07AxZKqC29aiyzw5MOQrZIhamVfeEvBRwvJUHojToCIQDosN7TQJ1/W5SHZgMqYwHb2kjG2jYZmitvxDrTJYzrlCr0AQij//////////8BEAAaDDU4MjUxNzM4NDI4OCIMN8/sOlBvM2OILze6KsgBKprIeB51ZoXmoE3jENdxjhSFzU7Iwj/vl5b+if9A966W550xca53x/707/ZIi68DBDx0/R9PAzhiUj8md5mT+WsRk+QC/396gjxL02qvPQqgKAFbzhy+c3OlXDlg4F8Kt7wtouvM8mDPKenRioFFep3zccYXR+LLDcj+5/RSq6jzGXD2HVNwKw7E6dFZm7KRZj/6QdvYh0ChRtSeg5sHqqqaGXsYvhBzHTxItiFpmCr39LsdEGI31Bi77u3HRkDdgi2f6CUpNY0w7Z+YnwY6lwHeS2CN1jG2frAh5ruoK4AkCdlgZ4bObNldEA0+/NBViz+ZDxV73untJUZlroLEl6sxndfLkmPdPTjneJf4sYJk7BwqifiYLxzLlYxM+p4AYOsab3+IfdyEKu+ZNems2hg9oknNkULKKty0PTGElcvDrMA4qFpFixqm7O5gZgWb0Qlf8UAJp3ps5SO6gqP3AzKvXqs5h4eN"
-                     )
+                     )  
  
 @app.route('/')
 def index():
@@ -35,7 +35,7 @@ def upload():
                     Filename=filename,
                     Key = filename
                 )
-                msg = " File Uploaded  ! "
+                msg = "File Uploaded  !"
 
     return render_template("index.html",msg =msg)
   
